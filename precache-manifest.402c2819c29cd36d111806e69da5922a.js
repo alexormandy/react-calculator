e992c785cdc9842c5954a379fe5fb389f80a9902
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-19 10:59:52 +0000

    Updates

---
+++ precache-manifest.402c2819c29cd36d111806e69da5922a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.6473c924.js"
+  },
+  {
+    "revision": "ee7643ca4ae49e46a643",
+    "url": "/react-calculator/static/js/main.ee7643ca.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/react-calculator/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "ee7643ca4ae49e46a643",
+    "url": "/react-calculator/static/css/main.f1b8b4fd.chunk.css"
+  },
+  {
+    "revision": "cb41b67870a6489eaa199d51f5ed51d8",
+    "url": "/react-calculator/index.html"
+  }
+];
--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Container from "./Container";
-import "./App.css";
-
-const App = () => {
-  return <Container />;
-};
-
-export default App;
