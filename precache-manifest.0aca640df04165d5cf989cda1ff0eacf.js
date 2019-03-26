ef829e254571b4c03192799fdc9a2de0a3de1fa9
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-26 10:38:56 +0000

    Updates

---
+++ precache-manifest.0aca640df04165d5cf989cda1ff0eacf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "2e31ecffc1255ede9bfb",
+    "url": "/react-calculator/static/js/main.d9c71260.chunk.js"
+  },
+  {
+    "revision": "5711b84e88abdf95a81e",
+    "url": "/react-calculator/static/js/2.f4c73c90.chunk.js"
+  },
+  {
+    "revision": "2e31ecffc1255ede9bfb",
+    "url": "/react-calculator/static/css/main.94e5e4f0.chunk.css"
+  },
+  {
+    "revision": "3dfe05413c2a2779d45825250e27db08",
+    "url": "/react-calculator/index.html"
+  }
+];
--- src/components/Display.test.js
+++
@@ -1,5 +0,0 @@
-const Display = require("./Display.js")
-
-test("changes values in state", () => {
-    expect()
-})
